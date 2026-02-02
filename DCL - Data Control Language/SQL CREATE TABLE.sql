c3dd3e1901038919d7cabc8c82b02fc8f6579abd
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-02-02 15:28:29 +0530

    Add files via upload

---
+++ DCL - Data Control Language/SQL CREATE TABLE.sql
@@ -0,0 +1,12 @@
+CREATE table table_name
+(
+Column1 datatype (size),
+column2 datatype (size),
+.
+.
+columnN datatype(size)
+);
+
+-- table_name: The name you assign to the new table.
+-- column1, column2, ... : The names of the columns in the table.
+-- datatype(size): Defines the data type and size of each column.
